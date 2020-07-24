419dbf02c1d3f6f3e1eff7c2457a8e9f8c0bff0a
Antoniox <antonioeh7@gmail.com>
2020-07-24 16:12:07 -0500

    Both Direction, and Magnitutde, VECTOR

--- Entity.cpp
+++ Entity.cpp
@@ -1,8 +1,10 @@
 #include "Entity.h"
 
 Entity::Entity(float nX, float nY, int nW, int nH, SDL_Texture* tex)
-	:x(nX), y(nY), texture(tex)
+	:texture(tex)
 {
+	transform.x = nX;
+	transform.y = nY;
 	currentFrame.x = 0;
 	currentFrame.y = 0;
 	currentFrame.w = nW;
--- Entity.h
+++ Entity.h
@@ -1,13 +1,13 @@
 #pragma once
 #include <SDL.h>
 #include <SDL_image.h>
-
+#include "Vector2D.h"
 
 class Entity
 {
 public:
 	Entity(float nX, float nY, int w, int h, SDL_Texture* tex);
-	float x, y;
+	Vector2D transform;
 	SDL_Texture* texture;
 	SDL_Rect currentFrame;
 private:
